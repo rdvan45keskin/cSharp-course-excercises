c487ac115eb879c51ac49c4e689c84e1de528b91
rdvan45keskin <135320556+rdvan45keskin@users.noreply.github.com>
2025-08-17 16:37:26 +0300

    Add files via upload

---
+++ WPF_Lessons/WPF_Tutorial_7_(MessageBox)/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Tutorial_7__MessageBox_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFire_Click(object sender, RoutedEventArgs e)
+        {
+            // MessageBox.Show("Could NOT Open this file","ERROR!",MessageBoxButton.OK,MessageBoxImage.Error);
+
+            MessageBoxResult result = MessageBox.Show("Do You Agree", "Agreement", 
+                                      MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                tbInfo.Text = "Agreed";
+            }
+            else
+            {
+                tbInfo.Text = "Denied";
+            }
+        }
+    }
+}
---
+++ WPF_Lessons/WPF_Tutorial_8_(Open_File_Dialog)/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Tutorial_8__Open_File_Dialog_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFire_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Music Files | *.mp4";
+            //openFileDialog.InitialDirectory = "D\\animeler";
+            openFileDialog.Title = "Please pick a mp4 file...";
+            openFileDialog.Multiselect = true;
+
+            bool? success = openFileDialog.ShowDialog();        //dosya seçme ekranı açmak için
+            if (success == true)
+            {
+                string[] paths = openFileDialog.FileNames;
+                string[] fileNames = openFileDialog.SafeFileNames;
+                //tbInfo.Text = fileName;
+
+                
+            }
+        }
+    }
+}
