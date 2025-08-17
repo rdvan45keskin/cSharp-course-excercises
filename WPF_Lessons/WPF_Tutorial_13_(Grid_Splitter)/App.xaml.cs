c981bc38c00bdae9b53f855e86787be4e93722b7
rdvan45keskin <135320556+rdvan45keskin@users.noreply.github.com>
2025-08-17 16:41:34 +0300

    Add files via upload

---
+++ WPF_Lessons/WPF_Tutorial_13_(Grid_Splitter)/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF_Tutorial_13__Grid_Splitter_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
