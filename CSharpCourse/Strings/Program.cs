69d5083346297104e47db70c68430dfc4f9a397e
rdvan45keskin <135320556+rdvan45keskin@users.noreply.github.com>
2025-08-17 17:03:10 +0300

    Add files via upload

---
+++ CSharpCourse/Strings/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string city = "Manisa";
+            ////Console.WriteLine(city[0]);
+
+            //foreach (var item in city)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //string city2 = "istanbul";
+            //string result = city +" "+city2;
+            //Console.WriteLine(result);
+            //Console.WriteLine(String.Format("{0} {1}", city, city2));
+
+            string sentence = "My name is Nicat Isler";
+            var result = sentence.Length;
+            var result2 = sentence.Clone();
+            //sentence = "My name is Selo";
+
+            var result3 = sentence.EndsWith("r");//r ile mi bitio
+            var result4 = sentence.StartsWith("M");//m ile mi başlıo
+            var result5 = sentence.IndexOf("name");//bulunduğu indexi söylüyo
+            var result6 = sentence.IndexOf(" ");//ilk bulduuğu şeyi verio
+            var result7 = sentence.LastIndexOf(" ");//sondan başlıyo
+            var result8 = sentence.Insert(0, "Hello ");//0. indexe verilen metni yerleştir
+            var result9 = sentence.Substring(3,4);//3. laralterden 4 karakter al
+            var result10 = sentence.ToUpper();//hepsini büyük harf yap
+            var result11 = sentence.ToLower();//hepsini küçük harf yap
+            var result12 = sentence.Replace(" ","-");//boşluk yerine - koy
+            var result13 = sentence.Remove(2,5);//2. indexten itibaren 5 index kaldır
+            Console.WriteLine(result13);
+        }
+    }
+}
---
+++ CSharpCourse/TypesAndVariables/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesAndVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Value Types
+            //Console.WriteLine("Hello World");
+            char character = 'a';
+            bool condition = true;
+            byte number4 = 255; //0-255 arası
+            short number3 = 32767;
+            int number1 = 2147483647;
+            long number2 = 9223372036854775807;
+            double number5 = 10.4;
+            decimal number6 = 1.1m;
+            var number7 = 10;//var int oluyo burda 10 olduğu için
+            
+            
+            Console.WriteLine("Number1 is {0}",number1);
+            Console.WriteLine("Number2 is {0}", number2);
+            Console.WriteLine("Number3 is {0}", number3);
+            Console.WriteLine("Number4 is {0}", number4);
+            Console.WriteLine("Number5 is {0}", number5);
+            Console.WriteLine("Number6 is {0}", number6);
+            Console.WriteLine("Character is : {0}",character);
+            Console.WriteLine(Days.Friday);
+            Console.ReadLine();
+        }
+    }
+
+    enum Days {
+        Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday
+    }
+}
