f834f987e68454e3df4761322000046247db4ecc
rdvan45keskin <135320556+rdvan45keskin@users.noreply.github.com>
2025-08-17 17:04:46 +0300

    Add files via upload

---
+++ DesignPatterns/AbstractFactory/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new Factory1());
+            productManager.GetAll();
+
+        }
+    }
+    public abstract class Logging 
+    {
+        public abstract void Log(string message);
+    }
+
+    public class XLogger : Logging
+    {
+        public override void Log(string message)
+        {
+            Console.WriteLine("Logged with XLogger");
+        }
+    }
+
+    public class YLogger : Logging
+    {
+        public override void Log(string message)
+        {
+            Console.WriteLine("Logged with YLogger");
+        }
+    }
+
+    public abstract class Caching
+    {
+        public abstract void Cache(string data);
+    }
+
+    public class XCache : Caching
+    {
+        public override void Cache(string data)
+        {
+            Console.WriteLine("Cached with XCache");
+        }
+    }
+
+    public class YCache : Caching
+    {
+        public override void Cache(string data)
+        {
+            Console.WriteLine("Cached with YCache");
+        }
+    }
+    //ana fabrika
+    public abstract class CrossCuttingConcernsFactory
+    {
+        public abstract Logging CreateLogger();
+        public abstract Caching CreateCaching();
+    }
+    //yancı fabrika1
+    public class Factory1 : CrossCuttingConcernsFactory
+    {
+        public override Logging CreateLogger()
+        {
+            return new XLogger();
+        }
+
+        public override Caching CreateCaching()
+        {
+            return new XCache();
+        }
+    }
+    //yancı fabrika2
+    public class Factory2 : CrossCuttingConcernsFactory
+    {
+        public override Logging CreateLogger()
+        {
+            return new YLogger();
+        }
+
+        public override Caching CreateCaching()
+        {
+            return new YCache();
+        }
+    }
+
+    public class ProductManager
+    {
+        CrossCuttingConcernsFactory _cccFactory;
+        Logging _logging;
+        Caching _caching;
+        public ProductManager(CrossCuttingConcernsFactory cccFactory)
+        {
+            _cccFactory = cccFactory;
+            _logging = _cccFactory.CreateLogger();
+            _caching = _cccFactory.CreateCaching();
+        }
+        public void GetAll()
+        {
+            _logging.Log("");
+            _caching.Cache("");
+            Console.WriteLine("Product Listed!");
+        }
+    }
+}
