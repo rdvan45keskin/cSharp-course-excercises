cc0cf66b80e04c3b7bc91c5a2d9be5973e0aea2b
rdvan45keskin <135320556+rdvan45keskin@users.noreply.github.com>
2025-08-17 16:33:39 +0300

    Add files via upload

---
+++ WPF_Lessons/WPF_Tutorial_2_(Code_Behind)/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF_Tutorial_2__Code_Behind_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
