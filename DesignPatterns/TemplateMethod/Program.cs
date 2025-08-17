881164deddbbbb2176e984f408a4743e745127cd
rdvan45keskin <135320556+rdvan45keskin@users.noreply.github.com>
2025-08-17 17:08:42 +0300

    Add files via upload

---
+++ DesignPatterns/TemplateMethod/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod
+{
+    //iç işlemleri soyutlayarak aynı işlemleri farklı değerler için çalıştırma
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ScoringAlgorithm algorithm;
+            Console.WriteLine("Man");
+            algorithm = new MenScoringAlgorithm();
+            Console.WriteLine(algorithm.GenerateScore(10, new TimeSpan(0, 2, 34)));
+
+            Console.WriteLine("Woman");
+            algorithm = new WomanScoringAlgorithm();
+            Console.WriteLine(algorithm.GenerateScore(10, new TimeSpan(0, 2, 34)));
+
+            Console.WriteLine("Child");
+            algorithm = new ChildrenScoringAlgorithm();
+            Console.WriteLine(algorithm.GenerateScore(10, new TimeSpan(0, 2, 34)));
+
+        }
+    }
+
+    abstract class ScoringAlgorithm
+    {
+        public int GenerateScore(int hits, TimeSpan time)
+        {
+            int score = CalculateBaseScore(hits);
+            int reduction = CalculateReduction(time);
+            return CalculateOverallScore(score, reduction);
+        }
+
+        public abstract int CalculateOverallScore(int score, int reduction);
+        public abstract int CalculateReduction(TimeSpan time);
+        public abstract int CalculateBaseScore(int hits);
+    }
+
+    class MenScoringAlgorithm : ScoringAlgorithm
+    {
+        public override int CalculateBaseScore(int hits)
+        {
+            return hits * 100;
+        }
+
+        public override int CalculateOverallScore(int score, int reduction)
+        {
+            return score - reduction;
+        }
+
+        public override int CalculateReduction(TimeSpan time)
+        {
+            return (int)time.TotalSeconds / 5;
+        }
+    }
+
+    class WomanScoringAlgorithm : ScoringAlgorithm
+    {
+        public override int CalculateBaseScore(int hits)
+        {
+            return hits * 100;
+        }
+
+        public override int CalculateOverallScore(int score, int reduction)
+        {
+            return score - reduction;
+        }
+
+        public override int CalculateReduction(TimeSpan time)
+        {
+            return (int)time.TotalSeconds / 3;
+        }
+    }
+
+    class ChildrenScoringAlgorithm : ScoringAlgorithm
+    {
+        public override int CalculateBaseScore(int hits)
+        {
+            return hits * 80;
+        }
+
+        public override int CalculateOverallScore(int score, int reduction)
+        {
+            return score - reduction;
+        }
+
+        public override int CalculateReduction(TimeSpan time)
+        {
+            return (int)time.TotalSeconds / 2;
+        }
+    }
+}
---
+++ DesignPatterns/Visitor/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor
+{
+    internal class Program
+    {
+        //herkese aynı anda işlem yapmaya yarıyo
+        static void Main(string[] args)
+        {
+            Manager nicat = new Manager {Name = "Nicat" , Salary = 50000};
+            Manager engin = new Manager { Name = "Engin", Salary = 30000 };
+
+            Worker selo = new Worker {Name = "Selo" , Salary = 1000 };
+            Worker musa = new Worker { Name = "Musa", Salary = 300 };
+            Worker ehucu = new Worker { Name = "Ehucu", Salary = 4000 };
+
+            nicat.Subordinates.Add(engin);
+            engin.Subordinates.Add(selo);
+            engin.Subordinates.Add(musa);
+            engin.Subordinates.Add(ehucu);
+
+            OrganisationalStructure organisationalStructure = new OrganisationalStructure(nicat);
+            PayrollVisitor payrollVisitor = new PayrollVisitor();
+            PayRiseVisitor payRiseVisitor = new PayRiseVisitor();
+
+            organisationalStructure.Accept(payrollVisitor);
+            organisationalStructure.Accept(payRiseVisitor);
+
+
+        }
+    }
+
+    class OrganisationalStructure
+    {
+        public EmployeeBase Employee;
+
+        public OrganisationalStructure(EmployeeBase firstEmployee)
+        {
+            Employee = firstEmployee;
+        }
+
+        public void Accept(VisitorBase visitor)
+        {
+            Employee.Accept(visitor);
+        }
+    }
+
+    abstract class EmployeeBase
+    {
+        public abstract void Accept(VisitorBase visitor);
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+    }
+
+    class Manager : EmployeeBase
+    {
+        public Manager() 
+        {
+            Subordinates = new List<EmployeeBase>();
+        }
+        public List<EmployeeBase> Subordinates { get; set; }
+        public override void Accept(VisitorBase visitor)
+        {
+            visitor.Visit(this);
+
+            foreach (var employee in Subordinates)
+            {
+                employee.Accept(visitor);
+            }
+        }
+    }
+
+    class Worker : EmployeeBase
+    {
+        public override void Accept(VisitorBase visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    abstract class VisitorBase
+    {
+        public abstract void Visit(Worker worker);
+        public abstract void Visit(Manager manager);
+    }
+    //ödeme yapma
+    class PayrollVisitor : VisitorBase
+    {
+        public override void Visit(Worker worker)
+        {
+            Console.WriteLine($"{worker.Name} paid {worker.Salary}");
+        }
+
+        public override void Visit(Manager manager)
+        {
+            Console.WriteLine($"{manager.Name} paid {manager.Salary}");
+        }
+    }
+    //maaş arttırma
+    class PayRiseVisitor : VisitorBase
+    {
+        public override void Visit(Worker worker)
+        {
+            Console.WriteLine($"{worker.Name} salary increased to {worker.Salary * (decimal)1.1}");
+
+        }
+
+        public override void Visit(Manager manager)
+        {
+            Console.WriteLine($"{manager.Name} salary increased {manager.Salary * (decimal)1.2}");
+
+        }
+    }
+}
